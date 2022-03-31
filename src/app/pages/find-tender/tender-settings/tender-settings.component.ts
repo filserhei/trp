d853c869107d3efbd4bac1c79d78456fb86d3010
s.filyotkin <serheifil@gmail.com>
2022-03-31 16:13:14 +0300

    settings

--- src/app/pages/find-tender/tender-settings/tender-settings.component.ts
+++ src/app/pages/find-tender/tender-settings/tender-settings.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TenderSettingsComponent implements OnInit {
   index = 0;
+  tableData: any[] = [{name: 'New announcement', }]
 
   constructor() { }
 
